d33254e85120e13fbd42454214aa7f0c7f58a05d
Gustavo Sandín <gustavo.sandin.sge@mango.com>
2025-03-27 14:56:44 +0100

    fix: lint

--- src/modules/list/ui/components/ListBreak/ListBreak.tsx
+++ src/modules/list/ui/components/ListBreak/ListBreak.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { useRouter } from 'next/navigation';
+
 import style from './ListBreak.module.scss';
 
 type ListBreakProps = {
@@ -8,14 +9,20 @@ type ListBreakProps = {
 };
 
 export function ListBreak({ line }: ListBreakProps) {
-const router = useRouter();
+  const router = useRouter();
   const back = () => {
     router.push('/aida');
   };
   return (
     <div className={style.container}>
       <section className={style.header}>
-        <img src={'/icons/back.svg'} alt='back' width='24' height='24' onClick={() => back()} />
+        <img
+          src={'/icons/back.svg'}
+          alt='back'
+          width='24'
+          height='24'
+          onClick={() => back()}
+        />
         <span>{line}</span>
       </section>
     </div>
