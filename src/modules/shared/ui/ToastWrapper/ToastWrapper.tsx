29174fd2b7bef780b7bbca045e3c4e569c5677ec
Gustavo Sandín <gustavo.sandin.sge@mango.com>
2025-03-27 21:52:29 +0100

    feat: toast redirect list page

--- src/modules/shared/ui/ToastWrapper/ToastWrapper.tsx
+++ src/modules/shared/ui/ToastWrapper/ToastWrapper.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import Image from 'next/image';
+import { useRouter } from 'next/navigation';
 import { useEffect, useState } from 'react';
 
 import { ToastProps } from '../../providers/ToastProvider/ToastProvider';
@@ -8,6 +9,7 @@ import { ToastProps } from '../../providers/ToastProvider/ToastProvider';
 import style from './ToastWrapper.module.scss';
 
 export const ToastWrapper = ({ description, displayTime }: ToastProps) => {
+  const router = useRouter();
   const [visible, setVisible] = useState(true);
 
   useEffect(() => {
@@ -18,10 +20,14 @@ export const ToastWrapper = ({ description, displayTime }: ToastProps) => {
     return () => clearTimeout(timer);
   }, [displayTime]);
 
+  const navigate = () => {
+    router.push('list/woman');
+  };
+
   if (!visible) return null;
 
   return (
-    <div className={style.container}>
+    <div className={style.container} onClick={() => navigate()}>
       <div className={style.image}>
         <Image
           alt='Aida'
